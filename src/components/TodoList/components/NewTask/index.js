b626b40bcde09fcf29108bd642f9f9113f90d5db
hang_nguyen92 <nguyenthihang100492@gmail.com>
2020-08-11 18:30:41 +0700

    add col, row

--- src/components/TodoList/components/NewTask/index.js
+++ src/components/TodoList/components/NewTask/index.js
@@ -1,10 +1,23 @@
 import React, { memo } from 'react';
 import { Main } from "./styled";
-const NewTask = props => {
+import { Card } from 'component/Cores';
+import FormItem from "components/FormItem";
+const NewTask = ({Input,
+  TextArea,
+  DatePicker,
+  Checkbox,
+  SelectCustom,
+  Card,
+  Button,}) => {
 
   return (
     <Main >
-
+      <Card>
+      <FormItem inputComponent={Input} placeholder="Add new task"/>
+      <FormItem inputComponent={TextArea} label="Discription"/>
+      <FormItem inputComponent={DatePicker} label="Due date"/>
+      <FormItem inputComponent={SelectCustom} label="Priority"/>
+      </Card>
     </Main>
   )
 } ;
